30d693f9e1b65d71c65f20d1288b7bf4dff49ea8
504BassSlapper <504BassSlapper@gmail.com>
2023-09-09 21:58:39 +0200

    add dependencies , use redis db

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask
+import time
+import redis
+
 
 
 app = Flask(__name__)
+cache = redis.Redis(host="redis", port=6379)
+
+def get_hit_count():
+    retries = 5 
+    while True:
+        try:
+            return cache.incr("hits")
+        except redis.exceptions.ConnectionError as exc:
+            if retries ==0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5) 
+
 
 @app.route('/')
 def home():
-    return('''
+    count = get_hit_count()
+    return(f'''
         <div>
-            <p> hello </p>
+            <p> hello, this web site has been consulted {count} </p>
         </div>
-           
            ''')
     
 if __name__ == "__main__":
